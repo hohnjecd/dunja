42226cab735bc4cc2e5ee0b73f1b449eece361a4
Dunja <hohnjecd.dh@gmail.com>
2016-08-25 19:59:22 +0200

    admin master page setup

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,37 @@ var elixir = require('laravel-elixir');
  |
  */
 
-elixir(function(mix) {
-    mix.sass('app.scss');
+ elixir(function(mix) {
+    mix.sass('app.scss')
+        .styles([
+
+        'libs/blog-post.css',
+        'libs/bootstrap.css',
+        'libs/bootstrap.min.css',
+        'libs/font-awesome.css',
+        'libs/metisMenu.css',
+        'libs/sb-admin-2.css',
+        'libs/styles.css'
+
+
+
+    ],'./public/css/libs.css')     //koristi se drugi parametar za lokaciju smestanja fajlova iz niza
+
+        .scripts([
+
+            'libs/bootstrap.js',
+            'libs/bootstrap.min.js',
+            'libs/jquery.js',
+            'libs/metisMenu.js',
+            'libs/sb-admin-2.js',
+            'libs/scripts.js'
+
+
+
+
+
+        ],'./public/js/libs.js')
+
+
+
 });
--- resources/views/admin/users/index.blade.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Dunja
- * Date: 8/19/2016
- * Time: 5:18 PM
- */
