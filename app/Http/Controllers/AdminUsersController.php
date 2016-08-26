7609f475fcec41c60b9653630b613fa3fad8b8dd
Dunja <hohnjecd.dh@gmail.com>
2016-08-26 11:59:09 +0200

    users able to persist

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UsersRequest;
+use App\Role;
+use App\User;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -15,9 +18,9 @@ class AdminUsersController extends Controller
      */
     public function index()
     {
-        //
+        $users=User::all();
 
-        return view('admin.users.index');
+        return view('admin.users.index',compact('users'));
 
 
     }
@@ -31,7 +34,10 @@ class AdminUsersController extends Controller
     {
         //
 
-        return view('admin.users.create');
+
+        $roles=Role::lists('name','id')->all();  //za konvertovanje u niz pri ispisu
+
+        return view('admin.users.create',compact('roles'));
     }
 
     /**
@@ -40,9 +46,16 @@ class AdminUsersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UsersRequest $request)
     {
         //
+
+    User::create($request->all());
+        return redirect('/admin/users');
+
+
+
+//        return $request->all();
     }
 
     /**
--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,7 +1,77 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Dunja
- * Date: 8/19/2016
- * Time: 5:21 PM
- */
+@extends('layouts.admin')
+
+
+
+@section('content')
+
+
+    <h1>Create Users</h1>
+
+
+
+    {!! Form::open(['method'=>'POST', 'action'=>'AdminUsersController@store','files'=>true]) !!}
+
+    <div class="form-group">
+
+        {!! Form::label('name','Name:') !!}
+        {!! Form::text('name', null, ['class'=>'form-control']) !!}
+
+
+    </div>
+
+    <div class="form-group">
+
+        {!! Form::label('email','email:') !!}
+        {!! Form::email('email', null, ['class'=>'form-control']) !!}
+
+
+    </div>
+
+    <div class="form-group">
+
+        {!! Form::label('role_id','Role:') !!}
+        {!! Form::select('role_id',[''=>'Choose Options']+ $roles , null, ['class'=>'form-control']) !!}
+
+
+    </div>
+
+    <div class="form-group">
+
+        {!! Form::label('is_active','Status:') !!}
+        {!! Form::select('is_active', array(1=>'Active',0=>'Not active'),0, ['class'=>'form-control']) !!}
+
+
+    </div>
+
+    <div class="form-group">
+
+        {!! Form::label('file','Title:') !!}
+        {!! Form::file('file', null, ['class'=>'form-control']) !!}
+
+
+    </div>
+
+
+    <div class="form-group">
+
+        {!! Form::label('password','Password:') !!}
+        {!! Form::password('password', ['class'=>'form-control']) !!}
+
+
+    </div>
+
+    <div class="form-group">
+
+    {!! Form::submit('Create Users',['class'=>'btn btn-primary']) !!}
+
+    </div>
+
+    {!! Form::close() !!}
+
+@include('includes.form_error')  <!-- fajl za greske ubacujemo-bolja varijanta da odvojino sve i samo zovemo po potrebi -->
+
+
+
+
+
+    @endsection
