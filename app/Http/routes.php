6476ce394475cb56265cd501f9daec931a81d163
Dunja <hohnjecd.dh@gmail.com>
2016-08-31 12:21:49 +0200

    categories and gitignore files

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -35,6 +35,7 @@ Route::group(['middleware'=>'admin'], function(){
 
     Route::resource('admin/users','AdminUsersController');    //resource --CRUD
     Route::resource('admin/posts','AdminPostsController');
+    Route::resource('admin/categories','AdminCategoriesController');
 
 
 
---
+++ resources/views/media/index.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dunja
+ * Date: 8/31/2016
+ * Time: 12:12 PM
+ */
